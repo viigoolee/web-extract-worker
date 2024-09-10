e9b11efb0a04943b044e0c45aceb5f5030d5324c
xuguo <xuguo@pinduoduo.com>
2024-09-10 09:13:03 +0800

    two models

--- extract.php
+++ extract.php
@@ -9,7 +9,6 @@ if ($prompt === '') {
 }
 
 $models = [
-    '@cf/lykon/dreamshaper-8-lcm',
     '@cf/stabilityai/stable-diffusion-xl-base-1.0',
     '@cf/bytedance/stable-diffusion-xl-lightning',
 ];
